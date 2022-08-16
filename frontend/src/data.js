f9528fdd420d348add377fe0df60c4371de77193
Luis Chavez <keichii.neko@gmail.com>
2022-08-16 17:24:25 -0500

    back end

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    prodcuts: [
+        {name: "Tenere 700",
+        slug:  " tenere 700",
+        category: "ADV Bikes",
+        image: "/images/tenere1.jpg",
+        price: 8000,
+        countInStock: 3,
+        brand: "Yamaha",
+        rating: 4.6,
+        numReviews: 10,
+        description: "great ADV",
+        },
+
+        {name: "African Twin 1200",
+        slug:  " African Twin",
+        category: "ADV Bikes",
+        image: "/images/african1.jpg",
+        price: 10000,
+        countInStock: 3,
+        brand: "Honda",
+        rating: 4.5,
+        numReviews: 7,
+        description: "great ADV",
+        },
+
+        {name: "BMW GS 1200",
+        slug:  " bmw gs ",
+        category: "ADV Bikes",
+        image: "/images/bmw1.jpg",
+        price: 8000,
+        countInStock: 3,
+        brand: "BMW",
+        rating: 4.6,
+        numReviews: 15,
+        description: "great ADV",
+        },
+    ]
+}
+
+export default data;
